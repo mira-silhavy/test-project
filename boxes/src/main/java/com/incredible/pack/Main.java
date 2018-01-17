5f2b8b01b18680e1267b1db0412e6983c20bdde4
Miroslav Šilhavý <mirek.silhavy@gmail.com>
2018-01-17 22:47:50 +1300

    Test commit

--- boxes/src/main/java/com/incredible/pack/Main.java
+++ boxes/src/main/java/com/incredible/pack/Main.java
@@ -1,6 +1,7 @@
 package com.incredible.pack;
 
 import java.math.BigDecimal;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -17,14 +18,31 @@ import com.incredible.pack.types.enums.EItemOrdering;
  */
 public class Main {
 
+    public final URL resource;
+    public final URL resource2;
+    public final URL resource3;
+    public final ClassLoader parent;
+    public final ClassLoader current;
+    private final Class thread;
+
+    Main() {
+        resource = Thread.currentThread().getContextClassLoader().getResource("test.yaml");
+        resource2 = this.getClass().getResource("test.yaml");
+        resource3 = this.getClass().getClassLoader().getResource("test.yaml");
+        current = this.getClass().getClassLoader();
+        parent = this.getClass().getClassLoader().getParent();
+        thread = Thread.currentThread().getClass();
+    }
+
     public static void main(String[] args) {
+        final Main main = new Main();
         final Scanner scan = new Scanner(System.in);
         final List<Item> items = new ArrayList<>();
 
         PackFactory packFactory = null;
 
         while (scan.hasNextLine()) {
-            final String line = scan.nextLine();
+            final String line = scan.nextLin  e();
 
             if (StringUtils.isEmpty(line)) {
                 break;
