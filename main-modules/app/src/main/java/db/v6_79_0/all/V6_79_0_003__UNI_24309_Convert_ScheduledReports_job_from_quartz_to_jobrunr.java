75ea0e80db39f21592d409ebbd16337448b3d702
Miroslav Silhavy <miroslav.silhavy@unimarket.com>
2023-04-23 22:09:22 +0200

    del

--- main-modules/app/src/main/java/db/v6_79_0/all/V6_79_0_003__UNI_24309_Convert_ScheduledReports_job_from_quartz_to_jobrunr.java
+++
@@ -1,9 +0,0 @@
-package main.java.db.v6_79_0.all;
-
-import com.unimarket.flyway.AbstractMigration;
-import com.unimarket.flyway.MigrationTemplate;
-import org.jetbrains.annotations.NotNull;
-
-public class V6_79_0_003__UNI_24309_Convert_ScheduledReports_job_from_quartz_to_jobrunr extends AbstractMigration {
-
-}
