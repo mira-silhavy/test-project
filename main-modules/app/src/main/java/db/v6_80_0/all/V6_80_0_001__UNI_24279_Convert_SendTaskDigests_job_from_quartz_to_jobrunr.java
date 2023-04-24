64768a9ad0ac46ff288964abb8c9220aa2a9b775
Miroslav Silhavy <miroslav.silhavy@unimarket.com>
2023-04-24 17:10:49 +0200

    move migrate

---
+++ main-modules/app/src/main/java/db/v6_80_0/all/V6_80_0_001__UNI_24279_Convert_SendTaskDigests_job_from_quartz_to_jobrunr.java
@@ -0,0 +1,7 @@
+package main.java.db.v6_80_0.all;
+
+import com.unimarket.flyway.AbstractMigration;
+
+public class V6_80_0_001__UNI_24279_Convert_SendTaskDigests_job_from_quartz_to_jobrunr extends AbstractMigration {
+
+}
