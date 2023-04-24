74411c1ac3702e6c98b6a82e8616659a839674c5
Miroslav Silhavy <miroslav.silhavy@unimarket.com>
2023-04-24 17:16:45 +0200

    fix branch

--- main-modules/app/src/main/java/db/v6_80_0/all/V6_80_0_001__UNI_24307_Convert_ScheduledTransactionRunJob_job_to_jobrunr.java
+++ main-modules/app/src/main/java/db/v6_80_0/all/V6_80_0_001__UNI_24307_Convert_ScheduledTransactionRunJob_job_to_jobrunr.java
@@ -1,11 +1,10 @@
-package db.v6_79_0.all;
-
-import org.jetbrains.annotations.NotNull;
+package main.java.db.v6_80_0.all;
 
 import com.unimarket.flyway.AbstractMigration;
 import com.unimarket.flyway.MigrationTemplate;
+import org.jetbrains.annotations.NotNull;
 
-public class V6_79_0_003__UNI_24307_Convert_ScheduledTransactionRunJob_job_to_jobrunr extends AbstractMigration {
+public class V6_80_0_001__UNI_24307_Convert_ScheduledTransactionRunJob_job_to_jobrunr extends AbstractMigration {
 
     @Override
     protected void doMigrate(@NotNull MigrationTemplate template) throws Exception {
