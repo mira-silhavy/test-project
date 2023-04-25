7c2395047644ecdf53615f67e7957f7751c181eb
Miroslav Silhavy <miroslav.silhavy@unimarket.com>
2023-04-25 13:30:47 +0200

    move migrate

---
+++ main-modules/app/src/main/java/db/v6_79_0/all/V6_79_0_003__UNI_24307_Convert_ScheduledTransactionRunJob_job_to_jobrunr.java
@@ -0,0 +1,8 @@
+package main.java.db.v6_79_0.all;
+
+import com.unimarket.flyway.AbstractMigration;
+import com.unimarket.flyway.MigrationTemplate;
+import org.jetbrains.annotations.NotNull;
+
+public class V6_79_0_003__UNI_24307_Convert_ScheduledTransactionRunJob_job_to_jobrunr extends AbstractMigration {
+}
