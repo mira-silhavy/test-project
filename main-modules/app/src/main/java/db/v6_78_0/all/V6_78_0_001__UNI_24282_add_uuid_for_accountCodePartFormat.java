90c7aa89ca8d03ecf7a9cb47ff49c8d0153e0d70
Miroslav Silhavy <miroslav.silhavy@unimarket.com>
2023-04-25 13:03:20 +0200

    remove

--- main-modules/app/src/main/java/db/v6_78_0/all/V6_78_0_001__UNI_24282_add_uuid_for_accountCodePartFormat.java
+++ main-modules/app/src/main/java/db/v6_78_0/all/V6_78_0_001__UNI_24282_add_uuid_for_accountCodePartFormat.java
@@ -7,14 +7,4 @@ import com.unimarket.flyway.MigrationTemplate;
 
 public class V6_78_0_001__UNI_24282_add_uuid_for_accountCodePartFormat extends AbstractMigration {
 
-    @Override
-    protected void doMigrate(@NotNull MigrationTemplate db) throws Exception {
-        LOGGER.info("UNI-24282 add UUID in ACCOUNT_CODE_PART_FORMAT - start");
-
-        db.execute("alter table ACCOUNT_CODE_PART_FORMAT add column UUID varchar(36) not null after SEQ;");
-        db.execute("update ACCOUNT_CODE_PART_FORMAT set UUID = UUID();");
-        db.execute("alter table ACCOUNT_CODE_PART_FORMAT add constraint UK_hmccjkr8u7dsd06bqdkh34fxx unique (UUID)");
-
-        LOGGER.info("UNI-24282 add UUID in ACCOUNT_CODE_PART_FORMAT - end");
-    }
 }
